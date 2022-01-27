e89b5c27abbce694fa16133cccb449e886654a16
jyj <abwjsduswls@naver.com>
2022-01-27 15:22:52 +0900

    url 수정, 폴더생성

--- seaview/views.py
+++ seaview/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Review
 
 # Create your views here.
 def index(request):
+    """
+    리뷰 출력
+    """
     return HttpResponse("Hello World")
