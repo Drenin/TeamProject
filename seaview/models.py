a194ae88f1cbc9f9916a5b689cbe42f1c8e78b33
jyj <abwjsduswls@naver.com>
2022-01-28 14:21:36 +0900

    모델 생성 함수 지정

--- seaview/models.py
+++ seaview/models.py
@@ -11,3 +11,10 @@ class Review(models.Model):
 
     def __str__(self):
         return self.postname
+
+class Reply(models.Model):
+    postname = models.ForeignKey(Review, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
--- seaview/views.py
+++ seaview/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Review
 
 # Create your views here.
 def index(request):
     """
-    리뷰 출력
+    리뷰 목록 출력
     """
-    return HttpResponse("Hello World")
+    review_list = Review.objects.order_by('-create_date')
+    content = {'review_list':review_list}
+    return render(request, 'seaview/review_list.html', content)
+    #return HttpResponse("Hello World")
+
+def detail(request, review_id):
+    """
+    리뷰 내용 출력
+    """
+    #review = Review.objects.get(id=review_id)
+    review = get_object_or_404(Review, pk=review_id)
+    content = {'review': review}
+    return render(request, 'seaview/review_detail.html', content)
