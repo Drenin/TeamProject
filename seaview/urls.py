86053d06b87be03055c2868563e656559b73cd24
jyj <abwjsduswls@naver.com>
2022-02-09 16:59:15 +0900

    '영화검색연결'

--- seaview/urls.py
+++ seaview/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+
 
 app_name = 'seaview'
 
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('<int:review_id>/', views.detail, name = 'detail'),
-    path('reply/create/<int:review_id>/', views.reply_create, name='reply_create'),
+    #path('reply/create/<int:review_id>/', views.reply_create, name='reply_create'),
+    path('search/', include('movieapi.urls')),
 ]
